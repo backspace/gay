194b096b80060a4605c02d479092bf189f854874
Buck Doyle <b@chromatin.ca>
2019-04-10 13:30:40 -0600

    Reduce file sizes
    I thought it was by line count, but it’s by bye size,
    according to https://github.com/github/linguist/issues/3987#issuecomment-357534567

--- brown.s
+++ brown.s
@@ -1,5 +1,4 @@
 section     .text
-global      _start                              ;must be declared for linker (ld)
 
 _syscall:
     int     0x80                                ;system call
@@ -10,13 +9,6 @@ _start:                                         ;tell linker entry point
 		;push   eax
     ;call   7:0
 
-	  add     esp,12                              ;clean stack (3 arguments * 4)
-
-	  push    dword 0                             ;exit code
-	  mov     eax,0x1                             ;system call number (sys_exit)
-	  call    _syscall                            ;call kernel
-
-		;we do not return from sys_exit,
 		;there's no need to clean stack
 section     .data
 
